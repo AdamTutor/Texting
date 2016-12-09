d7706f3d2ed106b4a7b041ea27f73aa36e61d223
Adam Tutor <atutor@basecampcodingacademy.org>
2016-12-09 12:39:52 -0600

    changed run.py

--- run.py
+++ run.py
@@ -1,29 +1,28 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 import api
-  
-  app = Flask(__name__)
-  
-  # Try adding your own number to this list!
-  callers = {
-     my_number: "Adam"
-  }
-  
-  @app.route("/", methods=['GET', 'POST'])
- def hello_monkey():
-     """Respond and greet the caller by name."""
- 
-     from_number = request.values.get('From', None)
-     if from_number in callers:
-         message = callers[from_number] + ", thanks for the message!"
-     else:
-         message = "Monkey, thanks for the message!"
- 
-     resp = twilio.twiml.Response()
-     resp.message(message)
- 
-     return str(resp)
- 
- if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+
+app = Flask(__name__)
+
+# Try adding your own number to this list!
+callers = {
+    my_number: "Curious George"
+}
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
+
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+
+    resp = twilio.twiml.Response()
+    resp.message(message)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
